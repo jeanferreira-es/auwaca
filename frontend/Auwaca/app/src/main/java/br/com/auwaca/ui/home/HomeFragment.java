5d9b650a1b7573e07be10e4ad589148c32fceef9
Jean Ferreira <jeanferreira.es@gmail.com>
2022-07-13 17:41:51 -0400

    Frontend da home screen implementado

--- frontend/Auwaca/app/src/main/java/br/com/auwaca/ui/home/HomeFragment.java
+++ frontend/Auwaca/app/src/main/java/br/com/auwaca/ui/home/HomeFragment.java
@@ -4,28 +4,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import br.com.auwaca.R;
 import br.com.auwaca.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
+    ImageView homeLogoWithName;
     private FragmentHomeBinding binding;
+    LinearLayout squad1;
+    LinearLayout squad2;
+    private LayoutInflater inflater;
+    private ViewGroup container;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
+        this.inflater = inflater;
+        this.container = container;
+
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        final TextView textView = binding.textHome;
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        homeLogoWithName = (ImageView) root.findViewById(R.id.home_logo);
+        homeLogoWithName.setImageResource(R.drawable.app_logo_with_name);
+
         return root;
     }
 
